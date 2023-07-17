e39c5bf51fc37aaab407146197c9a764094a3789
Deepak kumar sahoo <108812413+deepakgithub637@users.noreply.github.com>
2023-07-17 07:31:33 +0530

    Add files via upload

---
+++ about.php
@@ -0,0 +1,81 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>About Us - pariksha</title>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<link rel="stylesheet" type="text/css" href="css/aboutt.css">
+</head>
+<body>
+	<header>
+		<nav>
+			<ul>
+				
+				<li><a href="#" class="active">About Us </a></li>
+				
+			</ul>
+		</nav>
+	</header>
+    <body>
+
+		<div class="about-section">
+		  <h1>About Exam Instructions.</h1>
+		  <p>Arrange for stable Internet connectivity.
+		 Giving examination on Laptop or Desktop is highly recommended.
+		Make sure mobile/laptop is fully charged. Power bank for mobile or
+		UPS/Inverter for laptop/desktop should be arranged for uninterrupted
+		power supply.
+		Students should have sufficient data in Fair Usage Policy (FUP) / Internet
+		plan with sufficient data pack of internet service provider.
+		Login to the portal 10 min before the online examination start time.
+		Close all browsers/tabs before starting the online examination.
+		Once the exam starts, do not switch to any other window/tab. On doing so,
+		your attempt may be considered as malpractice and your exam may get
+		terminated.
+		Do Not Pickup/Receive the Call during the exam if you are giving the exam
+		on mobile. This also will be treated as changing the window.
+		To avoid unwanted pop-ups, use of Ad Blocker is recommended.
+		Clear browser cache memory on mobile and laptops. Clear browsing
+		history and also delete temp files...</p>
+		</div>
+		
+		<section class="team">
+			<div class="container">
+				<h2>Our Team</h2>
+				<p>Meet the team behind the examination Website:</p>
+				<div class="row">
+					<div class="col">
+						<img src="JPG/deepak.png" alt="Team Member">
+						<h3>Deepak Kumar Sahoo</h3>
+						<p>sipun993746@gmail.com</p>
+						<p>Team Leader</p>
+					</div>
+					<div class="col">
+						<img src="JPG/abtar.jpeg" alt="Team Member">
+						<h3>Abtar Singh Dandsena</h3>
+						<p>aarush.princesahil148@gmail.com</p>
+						<p>Member</p>
+					</div>
+					<div class="col">
+						<img src="JPG/mission.jpeg" alt="Team Member">
+						<h3>Mission Swain</h3>
+						<p>mission.guddu2001@gmail.com</p>
+						<p>Member</p>
+					</div>
+					<div class="col">
+						<img src="JPG/jee.jpg" alt="Team Member">
+						<h3>Jeebanjyoti Parida </h3>
+						<p>jeebanjyotiparida52@gmail.com</p>
+						<p> Member</p>
+					</div>
+				</div>
+			</div>
+		</section>
+	</main>
+	<footer>
+		<div class="container">
+			<p>&copy; 2023 Pariksha. All Rights Reserved.</p>
+		</div>
+	</footer>
+</body>
+</html>
---
+++ index.php
@@ -0,0 +1,125 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Online Examination Website</title>
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<link rel="stylesheet" href="css/index.css">
+    <link rel="stylesheet" href="css/fnewww.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css">
+    
+</head>
+<body>
+	<header>
+    
+		<h1 style="background-color:#28B463;">Smart  Examination System</h1>
+		<h2 style="background-color:#2E4053;">Take your knowledge to the next level</h2>
+		<table id="header" border="0" width="100%"
+        		cellpadding="0" cellspacing="0" bgcolor="#2E4053">
+                        <td>
+                            <a href="about.php" style="text-decoration:none">
+                                <font face="Verdana" size="5" color="#28B463">
+                                    About
+                                </font>
+                            </a>
+                        </td>
+ 
+                        <td>
+                            |
+                        </td>
+ 
+                        <td>
+                            <div id="reg">
+                                <font face="Verdana" size="5" color="#28B463">Register</font></a>
+                                <div id="drop">
+                                    <a href="teacher/registration.php" style="text-decoration:none">
+                                        Teacher</a>
+                                    
+                                    <a href="student.php" style="text-decoration:none">
+                                                Student</a>
+                                </div>
+
+                            </div>
+                            
+                        </td>
+ 
+                        <td>
+                            |
+                        </td>
+ 
+                        <td>
+                        <div id="reg">
+                                <font face="Verdana" size="5" color="#28B463">Login</font></a>
+                                <div id="drop">
+                                    <a href="teacher/login.php" style="text-decoration:none">
+                                        Teacher</a>
+                                    
+                                    <a href="login.php" style="text-decoration:none">
+                                                Student</a>
+                                </div>
+
+                            </div>
+                        </td>
+			<td>
+                            |
+                        </td>
+
+                        <td>
+                            <a href="contact.php" style="text-decoration:none">
+                                <font face="Verdana" size="5" color="#28B463">
+                                    Contact
+                                </font>
+                            </a>
+                        </td>
+                    </tr>
+                </table>
+	</header>
+	 <div class="container1">
+        <div class="box">
+            <img src="JPG/image-1.jpg" height="280";>
+            
+        </div>    
+        <div class="box">
+        <img src="JPG/image-2.jpg" height="280";>
+        
+    </div>
+    <div class="box">
+    <img src="JPG/image-3.jpg" height="280";>
+    
+</div>
+
+    </div>
+    
+
+   
+
+
+
+    <footer>
+        <div class="social">
+          <a href="http://www.instagram.com/abtar_singh"><i class="fab fa-instagram"></i></a>
+          <a href="http://www.facebook.com/Abtar Singh Dandsena"><i class="fab fa-facebook"></i></a>
+          <a href="#"><i class="fab fa-linkedin"></i></a>
+          <a href="#"><i class="fab fa-twitter"></i></a>
+        </div>
+        <ul class="list">
+       
+          <li>
+            <a href="about.php">About</a>
+          </li>
+          <li>
+            <a href="contact.php">Contact</a>
+          </li>
+          <li>
+            <a href="login.php">Login</a>
+          </li>
+        </ul>
+        <p class="copyright">
+            &copy; 2023 Pariksha, All Rights Reserved.
+        </p>
+    </footer>
+
+
+
+</body>
+
+</html>
